9b27882a9f98fe25669561f25110affacad06bd9
sluo6 <shangwenluo6@gmail.com>
2017-05-31 23:18:05 -0500

    API added

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,4 +1,10 @@
 
 public class Percolation {
-
+	public Percolation(int N)                // create N-by-N grid, with all sites initially blocked
+	   public void open(int row, int col)       // open the site (row, col) if it is not open already
+	   public boolean isOpen(int row, int col)  // is the site (row, col) open?
+	   public boolean isFull(int row, int col)  // is the site (row, col) full?
+	   public int numberOfOpenSites()           // number of open sites
+	   public boolean percolates()              // does the system percolate?
+	   public static void main(String[] args)   // unit testing (not required)
 }
