49a2e11aa1ee6cf6c1f3718b94ac9b34b16e0414
sluo6 <shangwenluo6@gmail.com>
2017-06-01 17:00:12 -0500

    basic data structure done!

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,11 +1,70 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	public Percolation(int N)                // create N-by-N grid, with all sites initially blocked
-	   public void open(int row, int col)       // open the site (row, col) if it is not open already
-	   public boolean isOpen(int row, int col)  // is the site (row, col) open?
-	   public boolean isFull(int row, int col)  // is the site (row, col) full?
-	   public int numberOfOpenSites()           // number of open sites
-	   public boolean percolates()              // does the system percolate?
-	   public static void main(String[] args)   // unit testing (not required)
+	private static int grid_size;       //grid size
+	private int[] pl;         //array data structure for percolation
+	private int n;        //number of open sites.
+	
+	/** create N-by-N grid, with all sites initially blocked.
+	 * Each site has three states: blocked, open, and full.
+	 * Each state is indicated by an integer. 
+	 * Blocked (0), Open (1), and Full (2).*/
+	public Percolation(int N) {
+		if (N <= 0)
+			throw new IllegalArgumentException("N should be larger than 0.");
+		else
+			this.grid_size = N;
+			int array_size = N * N;
+		    int[] pl = new int[array_size];
+		    for (int i = 0; i < N; i++) {
+			    pl[i] = 0;
+			this.pl = pl;
+			this.n = 0;
+		}
+	}
+	
+	public Percolation() {
+		this(grid_size);
+	}
+	//helper method to transform row and col info to array index.
+	private int index(int row, int col) {
+		int index = row  * this.grid_size + col;
+		return index;
+	}
+	
+	// open the site (row, col) if it is not open already   
+	public void open(int row, int col) {
+		int index_to_open = index(row, col);
+		if (pl[index_to_open] == 0) {
+			pl[index_to_open] = 1;
+			n = n + 1;
+		}
+	}
+	
+	// is the site (row, col) open?   
+	public boolean isOpen(int row, int col) {
+		int index_to_check = index(row, col);
+		if (pl[index_to_check] != 0)
+			return true;
+		else return false;		
+	} 
+	
+	// is the site (row, col) full?   
+	public boolean isFull(int row, int col) {
+		int index_to_check = index(row, col);
+		if (pl[index_to_check] == 2)
+			return true;
+		else return false;
+	}
+	
+	// number of open sites   
+	public int numberOfOpenSites() {
+		return n;
+	}  
+	
+	// does the system percolate?   
+	public boolean percolates() {
+		return true;
+	}           
+	  // public static void main(String[] args)   // unit testing (not required)
 }
