15cdb66317050eefce4b2e796f19f513add85b38
sluo6 <shangwenluo6@gmail.com>
2017-06-01 22:31:49 -0500

    cleaned git working tree

--- src/PercolationStats.java
+++
@@ -1,9 +0,0 @@
-
-public class PercolationStats {
-	public PercolationStats(int N, int T)   // perform T independent experiments on an N-by-N grid
-	   public double mean()                    // sample mean of percolation threshold
-	   public double stddev()                  // sample standard deviation of percolation threshold
-	   public double confidenceLow()           // low  endpoint of 95% confidence interval
-	   public double confidenceHigh()          // high endpoint of 95% confidence interval
-	
-}
